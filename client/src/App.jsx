8f1f4ed420375a289547ae80569e8b713f730201
KimKyungWoo94 <oj1370@naver.com>
2023-05-04 15:46:31 +0900

    배포 오류 수정 중

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,71 +26,74 @@ import {
 } from './api/loader';
 import PrivateRoute from './components/PrivateRoute';
 
-const router = createHashRouter([
-  {
-    path: '/',
-    element: <Root />,
-    loader: productsLoader,
-    children: [
-      {
-        path: PATH.MAIN,
-        loader: async () => ({
-          slides: await slidesLoader(),
-          pageProducts: await pageProductsLoader(),
-        }),
-        element: <Main />,
-      },
-      {
-        path: PATH.SIGNIN,
-        element: <SignIn />,
-      },
-      {
-        path: PATH.SIGNUP,
-        element: <SignUp />,
-      },
-      {
-        path: PATH.CART,
-        loader: cartsLoader,
-        element: <PrivateRoute redirectTo={PATH.SIGNIN} element={<Cart />} />,
-        errorElement: <PrivateRoute redirectTo={PATH.SIGNIN} element={<Cart />} />,
-      },
-      {
-        path: PATH.WISHLIST,
-        loader: favoritesLoader,
-        element: <PrivateRoute redirectTo={PATH.SIGNIN} element={<WishList />} />,
-        errorElement: <PrivateRoute redirectTo={PATH.SIGNIN} element={<WishList />} />,
-      },
-      {
-        path: PATH.CATEGORY,
-        loader: filteredProductsLoader,
-        element: <Category />,
-      },
-      {
-        path: `${PATH.PRODUCTS}/:id`,
-        loader: favoritesLoader,
-        element: <Products />,
-      },
-      {
-        path: PATH.ORDER,
-        loader: async () => ({
-          user: await userLoader(),
-          carts: await cartsLoader(),
-          coupons: await couponsLoader(),
-        }),
-        element: <PrivateRoute redirectTo={PATH.SIGNIN} element={<Order />} />,
-      },
-      {
-        path: PATH.ORDER_COMPLETE,
-        loader: historyLoader,
-        element: <PrivateRoute redirectTo={PATH.SIGNIN} element={<OrderComplete />} />,
-      },
-      {
-        path: '/*',
-        element: <NotFound />,
-      },
-    ],
-  },
-]);
+const router = createHashRouter(
+  [
+    {
+      path: '/',
+      element: <Root />,
+      loader: productsLoader,
+      children: [
+        {
+          path: PATH.MAIN,
+          loader: async () => ({
+            slides: await slidesLoader(),
+            pageProducts: await pageProductsLoader(),
+          }),
+          element: <Main />,
+        },
+        {
+          path: PATH.SIGNIN,
+          element: <SignIn />,
+        },
+        {
+          path: PATH.SIGNUP,
+          element: <SignUp />,
+        },
+        {
+          path: PATH.CART,
+          loader: cartsLoader,
+          element: <PrivateRoute redirectTo={PATH.SIGNIN} element={<Cart />} />,
+          errorElement: <PrivateRoute redirectTo={PATH.SIGNIN} element={<Cart />} />,
+        },
+        {
+          path: PATH.WISHLIST,
+          loader: favoritesLoader,
+          element: <PrivateRoute redirectTo={PATH.SIGNIN} element={<WishList />} />,
+          errorElement: <PrivateRoute redirectTo={PATH.SIGNIN} element={<WishList />} />,
+        },
+        {
+          path: PATH.CATEGORY,
+          loader: filteredProductsLoader,
+          element: <Category />,
+        },
+        {
+          path: `${PATH.PRODUCTS}/:id`,
+          loader: favoritesLoader,
+          element: <Products />,
+        },
+        {
+          path: PATH.ORDER,
+          loader: async () => ({
+            user: await userLoader(),
+            carts: await cartsLoader(),
+            coupons: await couponsLoader(),
+          }),
+          element: <PrivateRoute redirectTo={PATH.SIGNIN} element={<Order />} />,
+        },
+        {
+          path: PATH.ORDER_COMPLETE,
+          loader: historyLoader,
+          element: <PrivateRoute redirectTo={PATH.SIGNIN} element={<OrderComplete />} />,
+        },
+        {
+          path: '/*',
+          element: <NotFound />,
+        },
+      ],
+    },
+  ],
+  { basename: '/FENB_SHOPPING_MALL/' }
+);
 
 const App = () => <RouterProvider router={router} />;
 
